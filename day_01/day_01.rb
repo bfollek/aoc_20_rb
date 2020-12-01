4239c50120732de5fb8710e08008692976db46a9
Bob Follek <bfollek@gmail.com>
2020-12-01 06:47:54 -0500

    Adding sorbet

--- day_01/day_01.rb
+++ day_01/day_01.rb
@@ -1,13 +1,21 @@
+# typed: strict
+
+require 'sorbet-runtime'
+
 class Day01
+  extend T::Sig
 
   GOAL = 2020
+  NO_SOLUTION = "If we got here, we didn't find a solution."
 
+  sig { params(file_name: String).void }
   def initialize(file_name)
     @file_name = file_name
   end
 
   # "...find the two entries that sum to 2020 and then multiply those two numbers together."
   # part_1: 744475
+  sig { returns(Integer) }
   def part_1
     entries = load_entries
     entries.each_with_index do |entry_i, i|
@@ -21,10 +29,12 @@ class Day01
         end
       end
     end
+    raise NO_SOLUTION
   end
 
   # Find the _3_ entries that sum to 2020.
   # part_2: 70276940
+  sig { returns(Integer) }
   def part_2
     entries = load_entries
     entries.each_with_index do |entry_i, i|
@@ -40,8 +50,10 @@ class Day01
         end
       end
     end
+    raise NO_SOLUTION
   end
 
+  sig { returns(Integer) }
   def part_2_opt
     entries = load_entries
     entries.each_with_index do |entry_i, i|
@@ -59,12 +71,14 @@ class Day01
         end
       end
     end
+    raise NO_SOLUTION
   end
 
 # -------------------------------------------------------------------
   private
 # -------------------------------------------------------------------
 
+sig { returns(T::Array[Integer]) }
   def load_entries
     entries = []
     File.foreach(@file_name) do |line|
