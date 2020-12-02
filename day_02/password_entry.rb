9eb408425d01cba9db1e91c6860788b0c5865a46
Bob Follek <bfollek@gmail.com>
2020-12-02 08:57:31 -0500

    tc for PasswordEntry

--- day_02/password_entry.rb
+++ day_02/password_entry.rb
@@ -1,18 +1,26 @@
+# typed: strict
+
+require 'sorbet-runtime'
+
 class PasswordEntry
+  extend T::Sig
 
-  RE = /\A(\d+)-(\d+)\s+(\w):\s+(.*)\Z/
+  RE = /\A(?<min>\d+)-(?<max>\d+)\s+(?<required>\w):\s+(?<password>.*)\Z/
 
+  sig { params(line: String).void }
   def initialize(line)
     matches = line.match(RE)
     if matches
-      i, j, @required_char, @password = *matches[1..]
-      @min_count = i.to_i
-      @max_count = j.to_i
+      @min_count = T.let(matches[:min].to_i, Integer)
+      @max_count = T.let(matches[:max].to_i, Integer)
+      @required_char = T.let(matches[:required].to_s, String)
+      @password = T.let(matches[:password].to_s, String)
     else
       raise "Couldn't parse #{line}"
     end
   end
 
+  sig {returns(T::Boolean)}
   def part_1_valid?
     cnt = 0
     @password.each_char do |c|
@@ -22,6 +30,7 @@ class PasswordEntry
     cnt >= @min_count
   end
 
+  sig {returns(T::Boolean)}
   def part_2_valid?
     match_1 = @password[@min_count - 1] == @required_char
     match_2 = @password[@max_count - 1] == @required_char
