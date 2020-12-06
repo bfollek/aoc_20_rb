63e7d8a0d412b670b4089cbe52c002e729fb7d49
Bob Follek <bfollek@gmail.com>
2020-12-06 07:17:35 -0500

    Day 6 part 2 done

--- day_06/main.rb
+++ day_06/main.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
 # typed: true
 
-require_relative 'day_06'
+require_relative 'group'
 
-day_06 = Day06.new
-puts "part_1: #{day_06.part_1('../data/day_06.dat')}"
+groups = Group.load_from_file '../data/day_06.dat'
+
+# "For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?"
+i = groups.sum { |g| g.answers.size }
+# part_1: 6748
+puts "part_1: #{i}"
+
+# "For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?"
+cnt = 0
+groups.each do |g|
+  g.answers.each_value do |v|
+    cnt += 1 if v == g.num_members
+  end
+end
+# part_2: 3445
+puts "part_2: #{cnt}"
