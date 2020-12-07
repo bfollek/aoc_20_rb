b423b3cb24a69878d6be2a2ce19765c4f50add8d
Bob Follek <bfollek@gmail.com>
2020-12-07 13:34:16 -0500

    delegate

--- day_07/luggage.rb
+++ day_07/luggage.rb
@@ -1,33 +1,28 @@
+require 'delegate'
 
 require 'rgl/adjacency'
 require 'rgl/dot'
 require 'rgl/traversal'
 
-class Luggage
+class Luggage < SimpleDelegator # So that we 'inherit' the graph methods
 
   # "dotted bronze bags contain 2 muted tomato bags."
   # dark red bags contain 2 wavy beige bags, 1 clear bronze bag, 5 shiny coral bags, 3 shiny indigo bags."
   RE = /(?<color>.+) bags contain (?<contents>[^.]+)/ # Up to, but not including, the period.
 
-  attr_reader :graph
-
-  def initialize
-    @graph = RGL::DirectedAdjacencyGraph.new
-  end
-
   def draw
   end
 
   def self.load_from_file(file_name)
-    l = self.new
+    l = self.new(RGL::DirectedAdjacencyGraph.new)
     File.foreach(file_name) do |line|
       line = line.chomp
       matches = line.match(RE)
       if matches
         color = matches[:color]
-        l.graph.add_vertex(color) unless l.graph.has_vertex?(color)
+        l.add_vertex(color) unless l.has_vertex?(color)
         contents = matches[:contents]
-        #puts "#{color}, #{contents}, #{l.graph.num_vertices}"
+        puts "#{color}, #{contents}, #{l.num_vertices}"
       else
         raise "Can't parse #{line}"
       end
