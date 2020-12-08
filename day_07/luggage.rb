9ad6b801f5d30fbb9ed6e32bc1231cc2462974ec
Bob Follek <bfollek@gmail.com>
2020-12-07 19:35:03 -0500

    Added contents vertices, added edges, took out delegation

--- day_07/luggage.rb
+++ day_07/luggage.rb
@@ -1,40 +1,56 @@
-require 'delegate'
-
 require 'rgl/adjacency'
 require 'rgl/dot'
 require 'rgl/traversal'
 
-class Luggage < SimpleDelegator # So that we 'inherit' the graph methods
+class Luggage
 
   # "dotted bronze bags contain 2 muted tomato bags."
   # dark red bags contain 2 wavy beige bags, 1 clear bronze bag, 5 shiny coral bags, 3 shiny indigo bags."
   RE_LINE = /(?<color>.+) bags contain (?<contents>[^.]+)/ # Up to, but not including, the period.
 
+  # "light chartreuse bags contain no other bags."
+  NO_BAGS = "no other bags"
+
+  # "1 clear bronze bag" or "5 shiny coral bags"
+  RE_CONTENTS =/(?<count>\d+) (?<color>.+) bags?/
+
+  def initialize
+    @g = RGL::DirectedAdjacencyGraph.new
+  end
+
   def draw
   end
 
   def self.load_from_file(file_name)
-    l = self.new(RGL::DirectedAdjacencyGraph.new)
+    l = self.new
     File.foreach(file_name) do |line|
       line = line.chomp
       matches = line.match(RE_LINE)
       if matches
         l.store_luggage(matches[:color], matches[:contents])
       else
-        raise "Can't parse #{line}"
+        raise "Can't parse line #{line}"
       end
     end
     l
   end
 
-  def store_luggage(color, contents)
-    puts "#{color}, #{contents}, #{num_vertices}"
-    add_vertex(color)
+  def store_luggage(bag_color, contents)
+    puts "#{bag_color}, #{contents}, #{@g.num_vertices}"
+    @g.add_vertex(bag_color)
+    return if contents == NO_BAGS
     a = contents.split(", ")
-    #todo
-    #regex out number, vertex
-    # add vertex
-    # add number (weight)
+    a.each do |s|
+      matches = s.match(RE_CONTENTS)
+      if matches
+        content_color = matches[:color]
+        content_count = matches[:count].to_i
+        @g.add_vertex(content_color)
+        @g.add_edge(bag_color, content_color)
+      else
+        raise "Can't parse bag contents #{s}"
+      end
+    end
   end
 
 # -------------------------------------------------------------------
