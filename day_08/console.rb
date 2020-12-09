54a5282f16c5dc3337ed56ae24af02b9599a5178
Bob Follek <bfollek@gmail.com>
2020-12-09 12:21:55 -0500

    BF WIP

--- day_08/console.rb
+++ day_08/console.rb
@@ -2,25 +2,22 @@ require_relative 'instruction'
 
 class Console
 
-  def initialize(program)
-    load_code(program)
-  end
-
   # Return [true|false, acc] - true if the program terminated normally,
   # false if there was a loop and we killed it.
-  def run
+  def run(instructions)
     # Track which instructions run.
-    has_run = Array.new(@instructions.size, false)
+    has_run = Array.new(instructions.size, false)
     acc = 0
-    pc = 0 # Program Counter
-    while pc < @instructions.size
+    # Program counter.
+    pc = 0
+    while pc < instructions.size
       if has_run[pc]
-        # Loop - kill it
+        # Loop - kill it.
         return [false, acc]
       else
         has_run[pc] = true
       end
-      i = @instructions[pc]
+      i = instructions[pc]
       case i.code
       when Instruction::NO_OP
         pc += 1
@@ -37,18 +34,27 @@ class Console
   end
 
   def swap_codes_and_run(code_1, code_2)
+    insts = @instructions.dup
+    # dup instructions
+    # swap and try
+    # run - check how it ended
+    # restore instruction
+    # loop
+  end
+
+  def load_code(program)
+    instructions = []
+    File.foreach(program) do |cmd|
+      i = Instruction.new(cmd.chomp)
+      instructions << i
+    end
+    instructions
   end
 
 # -------------------------------------------------------------------
   private
 # -------------------------------------------------------------------
 
-  def load_code(program)
-    @instructions = []
-    File.foreach(program) do |cmd|
-      i = Instruction.new(cmd.chomp)
-      @instructions << i
-    end
-  end
+
 
 end
--- day_08/main.rb
+++ day_08/main.rb
@@ -3,8 +3,9 @@
 require_relative 'instruction'
 require_relative 'console'
 
-c = Console.new '../data/day_08.dat'
+c = Console.new
+instructions = c.load_code '../data/day_08.dat'
 
 # part_1: 1446
-_, acc = c.run
+_, acc = c.run instructions
 puts "part_1: #{acc}"
