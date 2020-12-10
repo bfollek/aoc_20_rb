0e4ef7ffb9d2e7c7c134bb2dde254c802339e834
Bob Follek <bfollek@gmail.com>
2020-12-09 19:24:12 -0500

    Tweak

--- day_09/xmas.rb
+++ day_09/xmas.rb
@@ -1,19 +1,19 @@
 class Xmas
 
   def initialize(file_name, preamble_size)
-    @preamble_size = preamble_size
-    @preamble_start = 0
     @nums = []
     File.foreach(file_name) { |line| @nums << line.chomp.to_i }
+    @preamble_size = preamble_size
   end
 
   # "The first step of attacking the weakness in the XMAS data is to find the first number in the list (after
   # the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not
   # have this property?"
   def invalid_num
+    preamble_start = 0
     loop do
-      target = @nums[@preamble_start + @preamble_size]
-      preamble = @nums[@preamble_start, @preamble_size]
+      target = @nums[preamble_start + @preamble_size]
+      preamble = @nums[preamble_start, @preamble_size]
       found_target = false
       preamble.each do |x|
         preamble.each do |y|
@@ -26,7 +26,7 @@ class Xmas
         break if found_target
       end
       return target if !found_target
-      @preamble_start += 1
+      preamble_start += 1
     end
   end
 
