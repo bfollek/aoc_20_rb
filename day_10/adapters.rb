cef486193cafe92a2001e82fc5a234c46775e86b
Bob Follek <bfollek@gmail.com>
2021-07-31 22:59:41 -0400

    Added some notes on day 10 part 2

--- day_10/adapters.rb
+++ day_10/adapters.rb
@@ -1,6 +1,9 @@
 # typed: false
 class Adapters
 
+  MIN_DIFF = 1
+  MAX_DIFF = 3
+
   def initialize file_name
     @adapters = []
     File.foreach(file_name) { |line| @adapters << line.chomp.to_i }
@@ -17,9 +20,9 @@ class Adapters
     prev = 0 # Starting joltage
     adapters.each do |a|
       case a - prev
-      when 1
+      when MIN_DIFF
         j1_diff += 1
-      when 3
+      when MAX_DIFF
         j3_diff += 1
       else
       end
@@ -27,5 +30,34 @@ class Adapters
     end
     j1_diff * j3_diff
   end
+
+  %{
+
+    I should be able to build the graph such that only nodes within range have edges.
+    nodes.each_with_index do |i, n|
+      nodes(i+1..).each do |other|
+        if other - MAX_JOLT <= n
+          add_edge(n, other)
+        else
+          break
+        end
+    end
+
+  # cnt = 0
+  # q = [start]
+  # while !q.empty?
+  #   v = q.pop
+  #   v.each_adjacent do |adj|
+  #     if adj == end
+  #       cnt += 1
+  #     else
+  #       q << adj
+  #     end
+  #   end
+  # end
+  # return cnt
+
+  The same node may show up multiple times. memoize its trip?
+  }
 
 end
