90caae7a4a588cfb9f2c58e7f6d278ec9e94b54d
Bob Follek <bfollek@gmail.com>
2021-08-01 08:15:17 -0400

    Working on day 10

--- day_10/adapters.rb
+++ day_10/adapters.rb
@@ -1,24 +1,26 @@
 # typed: false
 class Adapters
 
+  # "The charging outlet has an effective rating of 0 jolts..."
+  CHARGING_OUTLET = 0
   MIN_DIFF = 1
   MAX_DIFF = 3
 
   def initialize file_name
-    @adapters = []
+    @adapters = [CHARGING_OUTLET]
     File.foreach(file_name) { |line| @adapters << line.chomp.to_i }
-    # In addition, your device has a built-in joltage adapter rated for 3 jolts higher than the highest-rated
-    # adapter in your bag. (If your adapter list were 3, 9, and 6, your device's built-in adapter would be rated
-    # for 12 jolts.)
-    @adapters << @adapters.max + 3
+    @adapters.sort!
+    # "In addition, your device has a built-in joltage adapter rated for 3 jolts higher 
+    # than the highest-rated adapter in your bag. (If your adapter list were 3, 9, and 6, 
+    # your device's built-in adapter would be rated for 12 jolts.)"
+    @adapters << @adapters.last + 3
   end
 
-  def joltage_check
-    adapters = @adapters.sort
+  def joltage_differences
     j1_diff = 0
     j3_diff = 0
-    prev = 0 # Starting joltage
-    adapters.each do |a|
+    prev = CHARGING_OUTLET # Starting joltage
+    @adapters.each do |a|
       case a - prev
       when MIN_DIFF
         j1_diff += 1
--- tests/test_day_10.rb
+++ tests/test_day_10.rb
@@ -8,7 +8,7 @@ class Day10 < Minitest::Test
     dir = File.dirname(__FILE__)
     file_name = File.join(dir, "../testdata/day_10.dat")
     adapters = Adapters.new file_name
-    assert_equal 1848, adapters.joltage_check
+    assert_equal 1848, adapters.joltage_differences
   end
 
 end
